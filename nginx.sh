fbda47bb684dabc11b2ee5b503243012c095f20a
mingxuan <git4xuan@users.noreply.github.com>
2016-04-28 18:13:29 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,6 @@
 # move configurtion to nginx sites available
 cp ghost.conf /etc/nginx/sites-available/
-rm /etc/nginx/sites-available/default
+rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/ghost.conf /etc/nginx/sites-enabled/ghost.conf
 
 #service nginx start
