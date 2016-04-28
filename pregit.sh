7b41bbe6a81ec65cf3a513f63022a084c49baf83
mingxuan <git4xuan@users.noreply.github.com>
2016-04-28 15:31:51 +0800

    Update pregit.sh

--- pregit.sh
+++ pregit.sh
@@ -6,8 +6,10 @@ email=git4xuan+git@gmail.com
 ssh-keygen -t rsa -C "$email"
 echo `more ~/.ssh/id_rsa.pub`
 echo "add ssh pair to github or others"
+sleep 20
+
 git clone $location
 
 mkdir -p content
-cp -rf  ghost/images ghost/themes ghost/data ghost/apps ghost/config.js  content/*
+cp -rf  ghost/images ghost/themes ghost/data ghost/apps ghost/config.js  content/
 
