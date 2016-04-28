f5008551209987667a73cac147309c575fd91635
mingxuan <git4xuan@users.noreply.github.com>
2016-04-28 18:20:56 +0800

    Update letsEncrypt.sh

--- letsEncrypt.sh
+++ letsEncrypt.sh
@@ -1,3 +1,4 @@
 https://github.com/letsencrypt/letsencrypt
 cd letsencrypt/
 ./letsencrypt-auto
+./letsencrypt-auto certonly --standalone --email   git4xuan+lets@gmail.com -d blog.fengidea.com -d fengidea.com
